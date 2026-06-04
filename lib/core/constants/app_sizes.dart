4711f2eb1e7f90e7f8af38ab56528668fe7b3c4f
mdshakilhossain720 <mdshakilhossain720@gmail.com>
2026-06-04 09:59:20 +0600

    app size add

---
+++ lib/core/constants/app_sizes.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppSizes {
+  // Padding
+  static double p4 = 4.w;
+  static double p8 = 8.w;
+  static double p12 = 12.w;
+  static double p16 = 16.w;
+  static double p20 = 20.w;
+  static double p24 = 24.w;
+
+  // Radius
+  static double r8 = 8.r;
+  static double r12 = 12.r;
+  static double r16 = 16.r;
+
+  // Height
+  static double h10 = 10.h;
+  static double h20 = 20.h;
+  static double h30 = 30.h;
+  static double h40 = 40.h;
+
+  // Font
+  static double fs12 = 12.sp;
+  static double fs14 = 14.sp;
+  static double fs16 = 16.sp;
+  static double fs18 = 18.sp;
+  static double fs20 = 20.sp;
+}
